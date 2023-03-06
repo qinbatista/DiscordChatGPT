59e275b15ae001adb1d98a9431e67e5408540840
qin.batista <qin.batista@outlook.com>
2023-03-05 16:59:38 -0800

    update

--- App/DiscordLib/DiscordAPI.py
+++ App/DiscordLib/DiscordAPI.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 import discord
 import os
 from App.OpenAILib.OpenAI import chatgpt_response
-import asyncio
 load_dotenv()
 discord_token = os.getenv("DISCORD_TOKEN")
 
@@ -15,15 +14,9 @@ class MyClient(discord.Client):
         print("Logged on as {0}!".format(self.user))
 
     async def on_message(self, message):
-        # don't respond to themselves
-        if message.author == self.user:
+        if message.author == self.user:# don't respond to themselves
             return
-        # all message forward to OpenAI
         bot_response = chatgpt_response(prompt=message.content)
-        # try:
-        #     bot_response = await asyncio.wait_for(chatgpt_response(prompt=message.content), timeout=1.0)
-        # except asyncio.TimeoutError:
-        #     bot_response = "Sorry, OpenAI Sever is too busy, my brain can't reply you. 对不起，OpenAI服务器太忙了，我的智商无法回答你。"
         await self.reply_to_message(message, f"{bot_response}")
 
     async def direct_message(self, user, message):
